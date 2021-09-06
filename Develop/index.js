29bc6885300b9150fd31efa748507e17bd6ac5f4
Gerardo Sanchez <oracle942@gmail.com>
2021-09-06 11:20:24 -0700

    Added requires and prmopts

---
+++ Develop/index.js
@@ -0,0 +1,36 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs'); 
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: 'input',
+    name: 'usage',
+    message: "What will the application be used for?"},
+
+    {type: 'input',
+    name: 'instructions',
+    message: "How is the application used?"},
+
+    {type: 'input',
+    name: 'installation',
+    message: "Please provide instructinos for installing the application"},
+
+    {type: 'input',
+    name: 'issues',
+    message: "How are application issues reported?"},
+   
+    {type: 'input',
+    name: 'contributions',
+    message: "How can developers make contributions to the application?"},
+
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
